77d048d5110b464d076c9c62c16f11ac4967e3e0
Chiara <m.chiarapigoni@gmail.com>
2024-10-16 15:25:10 +0100

    add seeds into User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_encrypted :password, type: :string
+  # has_encrypted :password, type: :string
+  acts_as_google_authenticated lookup_token: :mfa_secret, encrypt_secrets: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.destroy_all
+
+users = User.create!([
+    { username: "Kamilya", password: "test1237320uge", email: "Kami@soemthing.com", MFA_secret: 456, google_secret: "nill" },
+    { username: "Khari", password: "test2346773209halsjS73", email: "Khari@soemthing.com", MFA_secret: 900, google_secret: "nill" },
+    { username: "Chiara", password: "testPL297278HAVD/", email: "Chiara@soemthing.com", MFA_secret: 655, google_secret: "nill" },
+    { username: "Mauro", password: "test786TTjaslasg", email: "Mauro@soemthing.com", MFA_secret: 499, google_secret: "nill" }
+])
+
+p "Created #{User.count} Users"
