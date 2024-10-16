973b14665b1a874c5f57aa479dc2d39f15971752
kharileigh <kharileighmiller@gmail.com>
2024-10-16 11:40:43 +0100

    Routes Created

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,37 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+
+
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root 'sessions#home'
+
+
+  # REGISTRATION 
+  resources :users, only: [:new, :create]
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+
+
+
+  # LOGIN ROUTES
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+
+
+  # 2FA GOOGLE
+  resources :user_mfa_sessions, only: [:new, :create]
+  get '/mfa', to: 'user_mfa_sessions#new'
+  post 'mfa', to: 'user_mfa_sessions#create'
+
+
+  # LOAN
+  resources :loans, only: [:new, :create]
+  get '/application', to: 'loans#new'
+  post '/application', to: 'loans#create'
+
 end
