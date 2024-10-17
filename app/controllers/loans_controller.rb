f250b2c1a85f3d03a5478b7be4ce6f66669e21b5
Chiara <m.chiarapigoni@gmail.com>
2024-10-17 15:00:29 +0100

    create user dashboard html page and application html page

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,7 +1,23 @@
 class LoansController < ApplicationController
   def new
+    render :application
   end
 
-  def create
-  end
+  # def create
+  #   @user = User.new(user_params)
+
+  #   if @user.save
+
+  #     # get user id
+  #     session[:user_id] = @user.id
+
+  #     # get username
+  #     session[:username] = @user.username
+
+  #     redirect_to user_path(@user.id)
+  #   else
+  #     render :new
+  #   end
+  # end
+
 end
