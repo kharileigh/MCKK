1d395e3267c0082934a0c866057c513b7cd4e823
kharileigh <kharileighmiller@gmail.com>
2024-10-20 13:34:56 +0100

    BOOTSTRAP - Welcome Page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_18_110633) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_18_111407) do
   create_table "loans", force: :cascade do |t|
     t.string "company_name"
     t.string "industry"
