f9e17afe35889e01d7f432cf35b59d937bc32241
maurobusso <mauro.busso12@gmail.com>
2024-10-23 11:55:33 +0100

    added favicon because why not

--- spec/controllers/user_mfa_sessions_controller_spec.rb
+++ spec/controllers/user_mfa_sessions_controller_spec.rb
@@ -1,40 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UserMfaSessionsController, type: :controller do
+    let(:user) { User.create(username: 'Kamilya', email: 'kamilya@example.com', password: 'password') }
 
-    describe 'GET #index' do
-        context 'when the user is logged in' do
-            before do
-                session[:user_id] = kamilya.id
-                get :index
-            end
-
-            it 'assigns @current_user to the logged-in user' do
-                expect(assigns(:current_user)).to eq(kamilya)
-            end
-
-            it 'assigns @loans to the current user’s loans' do
-                expect(assigns(:loans)).to eq(kamilya.loans)
-            end
-
-            it 'renders the index template' do
-                expect(response).to render_template(:index)
-            end
+    context "when user is logged in" do
+        before do
+            session[:user_id] = user.id
         end
-
-        context 'when no user is logged in' do
-            before do
-                get :index
-            end
-
-            it 'does not assign @current_user' do
-                expect(assigns(:current_user)).to be_nil
-            end
-
-            it 'redirects to the login page' do
-                expect(response).to redirect_to(login_path)
-            end
+        it 'create a valid qr code' do
+            get :new
+            expect(assigns(:current_user)).to eq(user)
+            expect(assigns(:link)).to be_present
         end
     end
 
+    context "when user is logged in" do
+        before do
+        session[:user_id] = user.id
+        allow(user).to receive(:google_authentic?).and_return(true) # Mocking the authentication check
+        end
+    end
 end
